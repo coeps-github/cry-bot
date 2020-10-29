a8d441cbb07b7806a4a3625f7e81365a98c1c303
Raphael Felber <coeps@coeps.com>
2020-10-29 19:14:45 +0100

    Added: UnitTests, simplified & fixed logic

--- src/statistics/statistics.spec.ts
+++ src/statistics/statistics.spec.ts
@@ -48,7 +48,7 @@ describe('statistics', () => {
               downCount: 0,
               hits: 0,
               upCount: 3,
-              win: 3
+              win: 2
             },
             {
               combination: {
@@ -57,7 +57,7 @@ describe('statistics', () => {
               downCount: 0,
               hits: 0,
               upCount: 3,
-              win: 3
+              win: 2
             },
             {
               combination: {
@@ -66,7 +66,7 @@ describe('statistics', () => {
               downCount: 0,
               hits: 0,
               upCount: 3,
-              win: 2
+              win: 1
             }
           ]
         });
@@ -134,6 +134,140 @@ describe('statistics', () => {
               hits: 0,
               upCount: 0,
               win: 0
+            }
+          ]
+        });
+        done();
+      });
+    });
+
+    it('should start counting when going down and then up', (done) => {
+      const statistics = getStatistics({
+        getTicks: () => from([
+          {
+            symbol: 'A',
+            open: '1',
+            close: '0'
+          },
+          {
+            symbol: 'A',
+            open: '0',
+            close: '1'
+          },
+          {
+            symbol: 'A',
+            open: '1',
+            close: '2'
+          }
+        ])
+      } as Binance);
+      const combinations = [
+        {
+          up: 0, down: 0
+        },
+        {
+          up: 1, down: 1
+        },
+        {
+          up: 2, down: 1
+        }
+      ];
+      statistics.analyzeCandles(undefined, undefined, combinations).pipe(skip(2)).subscribe(result => {
+        expect(result).toEqual({
+          A: [
+            {
+              combination: {
+                up: 0, down: 0
+              },
+              downCount: 0,
+              hits: 0,
+              upCount: 2,
+              win: 1
+            },
+            {
+              combination: {
+                up: 1, down: 1
+              },
+              downCount: 0,
+              hits: 0,
+              upCount: 2,
+              win: 1
+            },
+            {
+              combination: {
+                up: 2, down: 1
+              },
+              downCount: 0,
+              hits: 0,
+              upCount: 2,
+              win: 0
+            }
+          ]
+        });
+        done();
+      });
+    });
+
+    it('should stop counting when going up and then down', (done) => {
+      const statistics = getStatistics({
+        getTicks: () => from([
+          {
+            symbol: 'A',
+            open: '0',
+            close: '1'
+          },
+          {
+            symbol: 'A',
+            open: '1',
+            close: '2'
+          },
+          {
+            symbol: 'A',
+            open: '2',
+            close: '1'
+          }
+        ])
+      } as Binance);
+      const combinations = [
+        {
+          up: 0, down: 0
+        },
+        {
+          up: 1, down: 1
+        },
+        {
+          up: 2, down: 1
+        }
+      ];
+      statistics.analyzeCandles(undefined, undefined, combinations).pipe(skip(2)).subscribe(result => {
+        expect(result).toEqual({
+          A: [
+            {
+              combination: {
+                up: 0, down: 0
+              },
+              downCount: 1,
+              hits: 0,
+              upCount: 2,
+              win: 0
+            },
+            {
+              combination: {
+                up: 1, down: 1
+              },
+              downCount: 1,
+              hits: 0,
+              upCount: 2,
+              win: 0
+            },
+            {
+              combination: {
+                up: 2, down: 1
+              },
+              downCount: 1,
+              hits: 0,
+              upCount: 2,
+              win: -1
             }
           ]
         });
--- src/statistics/statistics.ts
+++ src/statistics/statistics.ts
@@ -20,22 +20,18 @@ export function getStatistics(binance: Binance): Statistics {
             const tickIsUp = up(tick);
             const combinationUp = statistic.combination.up > 0 ? statistic.combination.up : 1;
             const combinationDown = statistic.combination.down > 0 ? statistic.combination.down : 1;
-            const buy = statistic.upCount >= (combinationUp - 1);
-            const buyFirst = statistic.upCount === 0 && buy && tickIsUp;
-            const buyOne = statistic.upCount > 0 && buy;
-            const sell = buyOne && statistic.downCount >= (combinationDown - 1);
-            const sellFirst = statistic.downCount === 0 && sell && !tickIsUp;
-            const sellOne = statistic.downCount > 0 && sell;
-            const upCount = tickIsUp ? statistic.upCount + 1 : 0;
-            const downCount = buyOne && !tickIsUp ? statistic.downCount + 1 : 0;
-            const hits = buyOne && sellOne ? statistic.hits + 1 : statistic.hits;
-            const w = (buyFirst || buyOne) && !(sellFirst || sellOne) ? statistic.win + win(tick) : statistic.win;
+            const buy = statistic.upCount > (combinationUp - 1);
+            const sell = buy && statistic.downCount > (combinationDown - 1);
+            const upCount = tickIsUp ? statistic.upCount + 1 : buy ? statistic.upCount : 0;
+            const downCount = buy && !tickIsUp ? statistic.downCount + 1 : sell ? statistic.downCount : 0;
+            const hits = buy && sell ? statistic.hits + 1 : statistic.hits;
+            const w = buy && !sell ? statistic.win + win(tick) : statistic.win;
             return {
               ...statistic,
               hits,
               win: w,
-              upCount: buyOne && sellOne ? 0 : upCount,
-              downCount: buyOne && sellOne ? 0 : downCount
+              upCount: buy && sell ? 0 : upCount,
+              downCount: buy && sell ? 0 : downCount
             };
           });
           return {
