323f488381a903da57e9e080075409930d21766f
Raphael Felber <coeps@coeps.com>
2020-11-25 22:04:05 +0100

    Added: longPeriod name for filename

--- src/binance/helpers.ts
+++ src/binance/helpers.ts
@@ -1,5 +1,16 @@
 import { Period } from './model';
 
 export function getFileName(symbol: string, period: Period): string {
-  return `binance-${symbol}-${period}`;
+  const timeMap: { [key: string]: string } = {
+    m: 'minute',
+    h: 'hour',
+    d: 'day',
+    w: 'week',
+    M: 'month'
+  };
+  const key = period.substr(period.length - 1);
+  const number = +period.substring(0, period.length - 1);
+  const time = `${number}${timeMap[key]}`;
+  const longPeriod = number > 1 ? `${time}s` : time;
+  return `binance-${symbol}-${longPeriod}`;
 }
